fabdee02f4ef71d0ab749eb67135cb466a85d78c
caose <rlagkrwls258@gmail.com>
2024-09-04 11:12:18 +0900

    Lv1 task input implementation

--- src/calculatorLv1/App.java
+++ src/calculatorLv1/App.java
@@ -1,7 +1,20 @@
 package calculatorLv1;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        int a,b;
+        char operation;
+        System.out.print("첫 번째 숫자를 입력하세요: ");
+        a = sc.nextInt();
+        System.out.print("두 번째 숫자를 입력하세요: ");
+        b = sc.nextInt();
+        sc.nextLine();
+        System.out.print("사칙연산 기호를 입력하세요: ");
+
+        operation = sc.nextLine().charAt(0);
+        System.out.println(operation);
     }
 }
